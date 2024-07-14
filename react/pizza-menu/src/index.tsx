eedccdc5eb85f726973e73382ae38a2702e8caed
Martin Winther <martin.winther@protonmail.com>
2024-07-14 13:22:47 +0200

    add styling

--- react/pizza-menu/src/index.tsx
+++ react/pizza-menu/src/index.tsx
@@ -1,25 +1,69 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import pizzaData from './data'
+import './index.css'
+
 const App = () => {
 	return (
-		<div>
-			<h1>Hello World</h1>
-			<Pizza />
-			<Pizza />
-			<Pizza />
-			<Pizza />
-			<Pizza />
+		<div className="container">
+			<Header />
+			<Menu />
+			<Footer />
 		</div>
 	)
 }
 
+const Header = () => {
+	return (
+		<header className="header">
+			<h1>Fast React Pizza Co</h1>
+		</header>
+	)
+}
+
+const Menu = () => {
+	return (
+		<main className="menu">
+			<h2>Menu</h2>
+			<Pizza />
+			<Pizza />
+			<Pizza />
+			<Pizza />
+			<Pizza />
+		</main>
+	)
+}
+
+const Footer = () => {
+	const hour = new Date().getHours()
+	const openHour = 12
+	const closeHour = 22
+	const isOpen = hour >= openHour && hour <= closeHour
+
+	if (isOpen) {
+		return (
+			<footer className="footer">
+				<h3>Fast React Pizza Co.</h3>
+				<p>Open until {closeHour} </p>
+			</footer>
+		)
+	} else {
+		return (
+			<footer>
+				<h2>Fast React Pizza Co.</h2>
+				<p>Closed until {openHour} </p>
+			</footer>
+		)
+	}
+}
+
 const Pizza = () => {
 	return (
-		<div>
+		<div className="pizza">
 			<img src={pizzaData[0].photoName} alt="focaccia" />
-			<h2>{pizzaData[0].name}</h2>
+			<h4>{pizzaData[0].name}</h4>
 			<p>{pizzaData[0].ingredients}</p>
+			<p>{pizzaData[0].price}</p>
 		</div>
 	)
 }
