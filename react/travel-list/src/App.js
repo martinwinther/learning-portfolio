bb26b6dd5b27dc19ee0c41ce7bde05d92c1d2fa2
Martin Winther <martin.winther@protonmail.com>
2024-09-10 20:29:40 +0200

    update

--- react/travel-list/src/App.js
+++ react/travel-list/src/App.js
@@ -1,9 +1,34 @@
-function App() {
+export default function App() {
 	return (
 		<div className="App">
-			<header className="App-header"></header>
+			<Logo />
+			<Form />
+			<PackingList />
+			<Stats />
 		</div>
 	)
 }
 
-export default App
+function Logo() {
+	return <h1>🌴 Far Away 🧳</h1>
+}
+
+function Form() {
+	return (
+		<div className="add-form">
+			<h3>What do you need for your 😍 trip?</h3>
+		</div>
+	)
+}
+
+function PackingList() {
+	return <div className="list">LIST</div>
+}
+
+function Stats() {
+	return (
+		<footer>
+			<em>🧳 You have X items on your list, and you already packed X%</em>
+		</footer>
+	)
+}
