13623761b60eae9bc5f21bbf6c05a8af605560c8
martin winther <martin.winther@protonmail.com>
2024-11-23 09:00:24 +0100

    add new labs

---
+++ python/dojo/if-else/clean-text.py
@@ -0,0 +1,10 @@
+while True:
+    text = input('Text to Clean: ')
+    print(text.upper())
+    print(text.lower())
+    print(text.title())
+
+    text_clean = text.strip()
+
+    print(f'---{text}---')
+    print(f'---{text_clean}---')
