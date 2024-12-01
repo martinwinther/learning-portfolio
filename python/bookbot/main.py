34814a788b034767b27c2312c24b9c4f462bd53f
martin winther <martin.winther@protonmail.com>
2024-12-01 13:59:53 +0100

    add report

--- python/bookbot/main.py
+++ python/bookbot/main.py
@@ -1,5 +1,21 @@
-with open("books/frankenstein.txt") as f:
+def count_characters_frequency(text):
+    text = text.lower()
+    char_frequency = {}
+
+
+    for char in text:
+        if char != ' ':
+            char_frequency[char] = char_frequency.get(char, 0) + 1
+    return char_frequency
+
+book = "books/frankenstein.txt"
+with open(book) as f:
     file_contents = f.read()
 
-words = file_contents.split()
-print(len(words))
+character_counts = count_characters_frequency(file_contents)
+
+print(f"--- Begin report of {book} ---")
+for char, count in sorted(character_counts.items()):
+    print(f"The '{char}' character appears {count} times")
+
+print("--- End report ---")
